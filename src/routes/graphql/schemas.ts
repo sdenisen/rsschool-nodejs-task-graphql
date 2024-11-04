b9917e51ab1ffa72a96048dbfdcf9ccccf94f345
Sergey Denisenko <sdenisenko@five9.com>
2024-11-04 20:29:53 +0000

    feat: add types.

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,20 +1,61 @@
-import { Type } from '@fastify/type-provider-typebox';
+import {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLNonNull,
+    GraphQLBoolean,
+    GraphQLInt,
+    GraphQLEnumType,
+    GraphQLFloat,
+    GraphQLList
+} from 'graphql';
+import { UUIDType } from './types/uuid.js';
+import { MemberTypeId } from '../member-types/schemas.js';
 
-export const gqlResponseSchema = Type.Partial(
-  Type.Object({
-    data: Type.Any(),
-    errors: Type.Any(),
-  }),
-);
+const MemberTypeIdEnum = new GraphQLEnumType({
+    name: 'MemberTypeId',
+    values: {
+        BASIC: { value: MemberTypeId.BASIC },
+        BUSINESS: { value: MemberTypeId.BUSINESS },
+    },
+});
 
-export const createGqlResponseSchema = {
-  body: Type.Object(
-    {
-      query: Type.String(),
-      variables: Type.Optional(Type.Record(Type.String(), Type.Any())),
-    },
-    {
-      additionalProperties: false,
-    },
-  ),
-};
+const MemberType = new GraphQLObjectType({
+    name: 'Member',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+        discount: { type: new GraphQLNonNull(GraphQLFloat) },
+        postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+    }),
+});
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(UUIDType) },
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+});
+
+const ProfileType = new GraphQLObjectType({
+    name: 'Profile',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(UUIDType) },
+        isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+        yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+        memberType: { type: new GraphQLNonNull(MemberType) },
+    }),
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(UUIDType) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        balance: { type: new GraphQLNonNull(GraphQLFloat) },
+        profile: { type: ProfileType },
+        posts: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))) },
+        userSubscribedTo: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))) },
+        subscribedToUser: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))) },
+    }),
+});
